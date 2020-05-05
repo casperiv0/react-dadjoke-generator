a76c0d34d15e8299d13e101f4cffe61622a4c188
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-05 17:49:10 +0200

    Updates

---
+++ precache-manifest.8511d3bf84ee109e53b950e1d7e1140a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a24713e625241de1f8d783d8b0a1ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b0576ad0159c0e2e46",
+    "url": "/static/css/main.9022c78e.chunk.css"
+  },
+  {
+    "revision": "2c1d649a5b05d7450a8b",
+    "url": "/static/js/2.5f8e2612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f8e2612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b0576ad0159c0e2e46",
+    "url": "/static/js/main.cd59eeab.chunk.js"
+  },
+  {
+    "revision": "60f9e6e7109477797221",
+    "url": "/static/js/runtime-main.c396f2bd.js"
+  }
+]);
