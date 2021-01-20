8099923a82fb18a438f4a8bf7e48a4d4d52e63ad
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-01-20 16:50:44 +0100

    :tada: chore: update to React 17

--- src/App.js
+++ src/App.js
@@ -1,79 +1,72 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-const api = 'https://icanhazdadjoke.com/slack';
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+const api = "https://icanhazdadjoke.com/slack";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [dadJoke, setDadJoke] = useState("");
+  const [loading, setLoading] = useState(false);
+  const jokeRef = useRef();
 
-    this.state = {
-      dadJoke: '',
-      loading: true,
-    };
+  useEffect(() => {
+    generateNew();
+  }, []);
+
+  async function generateNew() {
+    setLoading(true);
+
+    await axios.get(api).then((res) => {
+      setDadJoke(res.data.attachments[0].fallback);
+    });
+
+    setLoading(false);
   }
 
-  generateNew = () => {
-    this.setState({ loading: true });
-    axios.get(api).then((res) => {
-      this.setState({
-        dadJoke: res.data.attachments[0].fallback,
-        loading: false,
-      });
-    });
-  };
-
-  copyToClipboard = async (event) => {
-    const joke = document.getElementById('joke').innerText;
-    const copyBtn = document.getElementById('copy');
+  function copyToClipboard() {
+    const joke = jokeRef.current?.textContent;
+    const copyBtn = document.getElementById("copy");
 
     if (!navigator.clipboard) {
-      return alert('navigator is not supported in this browser');
+      return alert("navigator is not supported in this browser");
     }
 
     // Thanks to https://codepen.io/flaviocopes/pen/yLBPaVY
     try {
       navigator.clipboard
         .writeText(joke)
-        .then((copyBtn.innerText = 'Copied'))
+        .then((copyBtn.innerText = "Copied!"))
         .then(
           setTimeout(() => {
-            copyBtn.innerText = 'Copy';
+            copyBtn.innerText = "Copy";
           }, 3000)
         );
     } catch (err) {
       console.error(err);
     }
-  };
-
-  componentDidMount() {
-    this.generateNew();
   }
 
-  render() {
-    const { dadJoke, loading } = this.state;
-    return (
-      <div>
-        <h1>DadJoke Generator</h1>
-        <a
-          href='https://github.com/dev-caspertheghost/react-dadjoke-generator'
-          target='_blank'
-          rel='noopener noreferrer'>
-          View Source Code
-        </a>
-        <div className='card'>
-          <div className='card-header'>
-            <span id='joke'>{loading ? 'Loading...' : dadJoke}</span>
-          </div>
-          <div className='card-footer'>
-            <button onClick={this.generateNew}>New Joke</button>
-            <button id='copy' onClick={this.copyToClipboard}>
-              Copy Joke
-            </button>
-          </div>
+  return (
+    <div>
+      <h1>DadJoke Generator</h1>
+      <a
+        href="https://github.com/dev-caspertheghost/react-dadjoke-generator"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View Source Code
+      </a>
+      <div className="card">
+        <div className="card-header">
+          <span ref={jokeRef}>{loading ? "Loading..." : dadJoke}</span>
+        </div>
+        <div className="card-footer">
+          <button onClick={generateNew}>New Joke</button>
+          <button id="copy" onClick={copyToClipboard}>
+            Copy Joke
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
